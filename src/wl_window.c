eddc98d5bc30599bb86c61fa3fa8ab86df696368
M374LX <wilsalx@gmail.com>
2026-08-25 18:53:53 -0300

    Revert "Make local pointer void" (it might incorrectly change the size when iterating)
    This reverts commit e8771b9e8591d1e498bf41b1a1b08f226362c6c0.

--- src/wl_window.c
+++ src/wl_window.c
@@ -727,7 +727,7 @@ static void _glfwXdgToplevelHandleConfigureWayland(void* userData,
                                                    struct wl_array* states)
 {
     _GLFWwindow* window = (_GLFWwindow*) userData;
-    void* state;
+    uint32_t* state;
 
     window->wl.pending.activated  = GLFW_FALSE;
     window->wl.pending.maximized  = GLFW_FALSE;
@@ -735,7 +735,7 @@ static void _glfwXdgToplevelHandleConfigureWayland(void* userData,
 
     wl_array_for_each(state, states)
     {
-        switch (*((uint32_t*) state))
+        switch (*state)
         {
             case XDG_TOPLEVEL_STATE_MAXIMIZED:
                 window->wl.pending.maximized = GLFW_TRUE;
